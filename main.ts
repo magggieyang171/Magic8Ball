9f9ac96fe1980b1016a393a0b8868f978341d992
magggieyang171 <116053282+magggieyang171@users.noreply.github.com>
2022-12-31 17:33:33 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let Number2 = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    Number2 = randint(1, 3)
+    if (Number2 == 3) {
+        basic.showIcon(IconNames.Yes)
+    } else if (Number2 == 2) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Meh)
+    }
+})
